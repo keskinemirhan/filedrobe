b951a45b8e6d149e626a1663f15d31caab22c1c8
cppmonger <keskinemirhan@gmail.com>
2023-05-11 13:20:53 +0300

    Add providers

--- filedrobe-backend/src/schematics/schematics.module.ts
+++ filedrobe-backend/src/schematics/schematics.module.ts
@@ -5,9 +5,17 @@ import { Tag } from "./entities/tag.entity";
 import { Group } from "./entities/group.entity";
 import { SchematicsService } from "./schematics.service";
 import { DriveModule } from "src/drive/drive.module";
+import { TagService } from "./tag.service";
+import { GroupService } from "./group.service";
+import { SchematicsInterceptor } from "./interceptor/schematics.interceptor";
 
 @Module({
   imports: [TypeOrmModule.forFeature([Schema, Tag, Group]), DriveModule],
-  providers: [SchematicsService],
+  providers: [
+    SchematicsService,
+    TagService,
+    GroupService,
+    SchematicsInterceptor,
+  ],
 })
 export class SchematicsModule {}
