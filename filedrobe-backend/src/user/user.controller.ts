c4b16d17b74eacc258516315e644bb084cb56e03
cppmonger <keskinemirhan@gmail.com>
2023-05-13 22:59:45 +0300

    minor reformat

--- filedrobe-backend/src/user/user.controller.ts
+++ filedrobe-backend/src/user/user.controller.ts
@@ -7,18 +7,17 @@ import {
   Req,
   UseGuards,
   UseInterceptors,
-} from '@nestjs/common';
-import { UserService } from './user.service';
-import { SCreateUserDto } from './sdto/s.create-user.dto';
-import { AuthGuard } from 'src/auth/guard/auth.guard';
-import { updateUserDto } from './dto/update-user.dto';
-@Controller('user')
+} from "@nestjs/common";
+import { UserService } from "./user.service";
+import { AuthGuard } from "src/auth/guard/auth.guard";
+import { updateUserDto } from "./dto/update-user.dto";
+@Controller("user")
 export class UsersController {
   constructor(private usersService: UserService) {}
-  static typeName = 'user';
+  static typeName = "user";
 
   //dev
-  @Get('all')
+  @Get("all")
   async getAllUser() {
     return await this.usersService.getAllUser();
   }
